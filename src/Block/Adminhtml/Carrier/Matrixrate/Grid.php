0944bdbb017063e8f25a1546e26cad126dffa8cd
wsagen <gen@webshopapps.com>
2016-01-07 10:36:25 +0800

    Updated license text

--- src/Block/Adminhtml/Carrier/Matrixrate/Grid.php
+++ src/Block/Adminhtml/Carrier/Matrixrate/Grid.php
@@ -1,16 +1,38 @@
 <?php
 /**
+ * WebShopApps
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * WebShopApps MatrixRate
+ *
+ * @category WebShopApps
+ * @package WebShopApps_MatrixRate
+ * @copyright Copyright (c) 2014 Zowta LLC (http://www.WebShopApps.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @author WebShopApps Team sales@webshopapps.com
+ *
+ */
+/**
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace WebShopApps\MatrixRate\Block\Adminhtml\Carrier\Matrixrate;
 
-/**
- * Shipping carrier table rate grid block
- * WARNING: This grid used for export table rates
- *
- * @author      Magento Core Team <core@magentocommerce.com>
- */
 class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
 {
     /**
--- src/README.md
+++
@@ -1,6 +0,0 @@
-The Magento_MatrixRate module implements the shipping methods which do not involve a direct interaction with shipping carriers, so called offline shipping methods. Namely, the following:
-*Free Shipping
-*Flat Rate
-*Table Rates
-*Store Pickup
-
