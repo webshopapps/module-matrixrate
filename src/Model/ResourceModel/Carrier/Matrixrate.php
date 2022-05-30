3a10b300ca62563e4130db75df1f2c8f08510b65
wsajason <jason.caster@shipperhq.com>
2022-05-30 15:29:40 +0100

    20.4.2 MNB-2539 Add support for Magento 2.4.4

--- src/Model/ResourceModel/Carrier/Matrixrate.php
+++ src/Model/ResourceModel/Carrier/Matrixrate.php
@@ -204,7 +204,14 @@ class Matrixrate extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     {
         $adapter = $this->getConnection();
         $shippingData=[];
-        $postcode = trim($request->getDestPostcode()); //SHQ18-1978
+
+        $postcode = trim($request->getDestPostcode() ?? ""); //SHQ18-1978
+
+        if ($request->getDestCountryId() == 'BR') {
+            #  Brazil can have a hyphen, let's remove it
+            $postcode = trim(str_replace("-","", $postcode));
+        }
+
         if ($zipRangeSet && is_numeric($postcode)) {
             #  Want to search for postcodes within a range. SHQ18-98 Can't use bind. Will convert int to string
             $zipSearchString = ' AND ' . (int)$postcode . ' BETWEEN dest_zip AND dest_zip_to ';
@@ -506,7 +513,7 @@ class Matrixrate extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 
         // strip whitespace from the beginning and end of each row
         foreach ($row as $k => $v) {
-            $row[$k] = trim($v);
+            $row[$k] = trim($v ?? "");
         }
 
         // validate country
